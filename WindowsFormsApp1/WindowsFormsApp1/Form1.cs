69590c6261dc29e1dda87ae1fe2218a42c7ce54b
Jaxton Lee <jaxton@flexsoftware.com.my>
2021-08-26 21:56:12 +0800

    1

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -16,7 +16,9 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
 
-            var test = 1;
+            var test = 2;
+
+            var aaa = "asd";
         }
     }
 }
